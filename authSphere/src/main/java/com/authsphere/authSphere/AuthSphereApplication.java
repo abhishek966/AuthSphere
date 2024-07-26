52d0b44ab3df8220ffedc95a09c229d75a3ab1e4
abhishek966 <abhishekdutt79@yahoo.com>
2024-07-26 22:52:39 +0530

    setup project
    initial setup

---
+++ authSphere/src/main/java/com/authsphere/authSphere/AuthSphereApplication.java
@@ -0,0 +1,13 @@
+package com.authsphere.authSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSphereApplication.class, args);
+	}
+
+}
